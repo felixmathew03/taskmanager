a678bff8e9b55f4bb37b4c7048e7255b6274da39
felixmathew03 <felixmathewtt@gmail.com>
2025-11-03 10:11:10 +0530

    static files and gunicorn for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn taskmanger.taskmanager.wsgi
--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-@5%#a)yu3a^%#5@96%(ng6c9%-z&u==h%qzb*2sx^%npiw$8_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
